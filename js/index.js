01ce0724f16dc7f1ca97acc149386c166d78bcd0
Игнат Лубнин <lubnin.ignat.s@gmail.com>
2021-07-12 12:40:01 +0500

    добавил паузу

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 let lable = document.querySelector("#lable");
 let btnStart = document.querySelector("#btnStart");
 let btnStop = document.querySelector("#btnStop");
+let btnPause = document.querySelector("#btnPause");
+let btnPlay = document.querySelector("#btnPlay");
 let textBox = document.querySelector("#textBox");
 let speed = document.querySelector("#speed");
 
+btnPause.hidden = true;
+btnPlay.hidden = true;
+btnStop.hidden = true;
 
 let index = 0;
 let timerId = 0;
@@ -11,23 +16,44 @@ let textArr = {};
 
 function start() {
     clearTimeout(timerId);
-    index = 0;
     textArr = textBox.value.split(" ");
-   // textBox.hidden=true;
+
+    btnStart.hidden = true;
+    textBox.hidden = true;
+
+    btnPause.hidden = false;
+    btnStop.hidden = false;
+
+    play()
+}
+
+function stop() {
+    lable.innerText = "Введи текст и нажми Старт"
+    btnStart.hidden = false;
+    textBox.hidden = false;
+
+    btnPlay.hidden = true;
+    btnPause.hidden = true;
+
+    clearTimeout(timerId)
+}
+
+function pause() {
+    btnPlay.hidden = false;
+    btnPause.hidden = true;
+    clearTimeout(timerId)
+}
+
+function play() {
+    btnPlay.hidden = true;
+    btnPause.hidden = false;
+
     timerId = setInterval(() => {
-        if (index == textArr.length) {            
+        if (index == textArr.length) {
             stop();
-            lable.innerText = "Введи текст и нажми Старт"
             return;
         }
         lable.innerText = textArr[index]
         index += 1;
     }, parseInt(speed.value) || 200);
 }
-
-function stop() {
-    //textBox.hidden=false;
-    clearTimeout(timerId)
-    
-}
-
